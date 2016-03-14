967169ec30c2dbad6a02183ebbd05a7072439da3
Edson Ribeiro Junior <edribeirojunior@gmail.com>
2016-03-14 11:11:22 -0300

    File exists
    ##Alterado de -s para -e,  pois este é o indicador de file exists

--- AWS/aws-instances.sh
+++ AWS/aws-instances.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -s /usr/local/scripts/instances ]; then
+if [ -e /usr/local/scripts/instances ]; then
     echo "Updating instances ID's"
     /usr/local/bin/aws ec2 describe-tags --filters "Name=tag:Startup,Values=1" | tr [:upper:] [:lower:] | tr -d '"' | grep resourceid | tr -d ',' | awk -F: '{print $2}' >> /usr/local/scripts/instances
 
@@ -9,12 +9,10 @@ else
     echo "Creating Instances File"
     touch /usr/local/scripts/instances
 
-    if [ -s /usr/local/scripts/instances ]; then
+    if [ -e /usr/local/scripts/instances ]; then
            echo "Updating instances ID's"
            /usr/local/bin/aws ec2 describe-tags --filters "Name=tag:Startup,Values=1" | tr [:upper:] [:lower:] | tr -d '"' | grep resourceid | tr -d ',' | awk -F: '{print $2}' >> /usr/local/scripts/instances
     fi
 fi
-
-
 
 
