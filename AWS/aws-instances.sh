d656f6ef049d40fc42537fb908ab0cec65f953d0
Edson Ribeiro Junior <edribeirojunior@gmail.com>
2016-03-15 14:25:33 -0300

    Clean instance file

--- AWS/aws-instances.sh
+++ AWS/aws-instances.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 if [ -e /usr/local/scripts/instances ]; then
-    echo "Updating instances ID's"
-    /usr/local/bin/aws ec2 describe-tags --filters "Name=tag:Startup,Values=1" | tr [:upper:] [:lower:] | tr -d '"' | grep resourceid | tr -d ',' | awk -F: '{print $2}' >> /usr/local/scripts/instances
-
+    echo "Cleaning instances file..."
+    cp /dev/null /usr/local/scripts/instances
+    echo "Updating instances ID's..."
+    /usr/bin/aws ec2 describe-tags --filters "Name=tag:Shutdown,Values=1" | tr [:upper:] [:lower:] | tr -d '"' | grep resourceid | tr -d ',' | awk -F: '{print $2}' >> /usr/local/scripts/instances
+    echo "Instance File Updated"
 else
 
     echo "Creating Instances File"
     touch /usr/local/scripts/instances
 
     if [ -e /usr/local/scripts/instances ]; then
-           echo "Updating instances ID's"
-           /usr/local/bin/aws ec2 describe-tags --filters "Name=tag:Startup,Values=1" | tr [:upper:] [:lower:] | tr -d '"' | grep resourceid | tr -d ',' | awk -F: '{print $2}' >> /usr/local/scripts/instances
+   echo "Updating instances ID's..."
+       /usr/bin/aws ec2 describe-tags --filters "Name=tag:Shutdown,Values=1" | tr [:upper:] [:lower:] | tr -d '"' | grep resourceid | tr -d ',' | awk -F: '{print $2}' >> /usr/local/scripts/instances
+   echo "Instance File Updated"
     fi
 fi
 
