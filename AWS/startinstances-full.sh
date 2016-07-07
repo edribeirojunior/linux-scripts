9cb55174ac65a35c7f7fe98056c9abebddd4436e
Edson Ribeiro Junior <edribeirojunior@gmail.com>
2016-07-07 17:31:52 -0300

    stop instances
    Stop  Instances with no other needed files.

---
+++ AWS/startinstances-full.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+#Set Shell Variable SML, runs command where TAG SML is equal "1".  
+SML=$(/usr/bin/aws ec2 describe-tags --filters "Name=tag:SML,Values=1" | tr [:upper:] [:lower:] | tr -d '"' | grep resourceid | tr -d ',' | awk -F: '{print $2}')
+
+# Stop instances 
+/usr/bin/aws ec2 start-instances --instance-ids $SML
